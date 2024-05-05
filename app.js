8e60e1ffba89af78a8d06bffe0afae1426a9a3b0
turtle-jin <codingjinjin@gmail.com>
2024-05-05 18:45:24 -0500

    basic design and initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+//find sum
+function sum(a, b) {
+    return a + b; 
+}
+
+//find difference
+function subtract(a, b) {
+    return a - b; 
+}
+
+//find product 
+function multiply(a, b) {
+    return a * b; 
+}
+
+//find quotient 
+function divide(a, b) {
+    if( b === 0) {
+        return "CANNOT DIVIDE BY 0"; 
+    }else {
+      return a / b;   
+    }   
+}
+
+//find power
+function power (base, power) {
+    let result = 1
+    for(let i = 0; i < power; i++) {
+        result *= base; 
+    }
+    return result; 
+}
